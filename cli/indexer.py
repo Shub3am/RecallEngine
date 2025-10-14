d2b19eda1248b11dae0f0dea3789b321bbb52949
Shubham Vishwakarma <shubham@vshubham.com>
2025-10-14 14:47:52 +0530

    indexer ideation

---
+++ cli/indexer.py
@@ -0,0 +1,11 @@
+class Indexer:
+    def __init__(self, docmap, index) -> None:
+        self.doc_map = docmap
+        self.index = index
+        
+    def __add_document(self, doc_id, text):
+        pass
+    def get_document(self, term):
+        pass
+    def build():
+        pass
